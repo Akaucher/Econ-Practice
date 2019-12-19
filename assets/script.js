40cd3a9ab3bd1ae6126df891a35c9edf469099d1
Suspicious <alex@kaucher.com>
2019-12-19 14:01:25 -0700

    Currency data up and running

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+
+
+d3.select("#test").style("background-color", "black");
+
+$("#button").on("click", function yes(){
+    let settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://currency-converter5.p.rapidapi.com/currency/convert?format=json&from=AUD&to=USD&amount=1",
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-host": "currency-converter5.p.rapidapi.com",
+            "x-rapidapi-key": "85950a923emsh59a1398139e0405p186b84jsnfbe7a90cc117"
+        }
+    }
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
+});
